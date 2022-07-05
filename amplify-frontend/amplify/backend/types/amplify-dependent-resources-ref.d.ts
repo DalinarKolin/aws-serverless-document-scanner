b08760cc82646836ebb481dafad05e0ca680ca05
EC2 Default User <ec2-user@ip-172-31-8-204.us-east-2.compute.internal>
2022-07-05 13:29:51 +0000

    cheng name for Octank

---
+++ amplify-frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "documentscannerb870f318": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    }
+}
